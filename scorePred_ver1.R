d5b1bc51d21fb809889bc2583e5e9c47f36bee04
amitav sahay <amitav.sahay@gmail.com>
2018-08-27 23:46:28 -0500

    Data for exploration is now written out in 2 files: 1 with a row for each ball (detail), and the other with a row for each innings (summary)

--- scorePred_ver1.R
+++ scorePred_ver1.R
@@ -31,10 +31,44 @@ matDet <- matDet %>% select (Team_Batting, Season, Match_id = MatcH_id, Innings_
                              Runs_Scored, Extra_runs, Bowler_Wicket, Run_out, Team_Bowling)  %>%
   arrange (Team_Batting, Season, Match_id, Innings_No, Over_id, Ball_id) %>%
   group_by (Team_Batting, Season, Match_id, Innings_No) %>%
-  mutate (cumRuns = cumsum(Runs_Scored + Extra_runs), cumWkts = cumsum(Bowler_Wicket + Run_out)) %>%
+  mutate (cumOver = (Over_id - 1) + 
+            round(ifelse (Ball_id > 6, 0.0, (Ball_id %% 6) / 6), 2) +   # If an over had extra balls, count them as 6 balls
+            ifelse(Ball_id >= 6, Ball_id %/% 6, 0 )) %>%
+  mutate (cumRuns = cumsum(Runs_Scored + Extra_runs), 
+          runRate = round (cumRuns / cumOver, 2), 
+          cumWkts = cumsum(Bowler_Wicket + Run_out)) %>%
   select (-c(Runs_Scored, Extra_runs, Bowler_Wicket, Run_out)) %>% # Discard columns not needed anymore since cumRuns and cumWkts are built
-  filter (Innings_No < 3)  # Only interested in full innings 1 and 2; not super over innings to break tie
-  
+  filter (Innings_No < 3) %>%   # Only interested in full innings 1 and 2; not super over innings to break tie
+  left_join(matSumm, by = c("Match_id" = "match_id")) %>%
+    select(Team_Batting:cumWkts, venueGround = Venue_Name, venueCity = City_Name, winner = match_winner)
+
+# Now add the batting and bowling team names with the lookup table team; Trouble with data "as is" is that 
+# seasons 2008-2016 has numbers in the batting and bolwing team ids, but for season 2017, the actual team names are stored
+# So do the lookup in 2 steps: first for the nueric ids (they actually are factor data types)
+
+matDetNum <-matDet %>% mutate (newBatTeamId = as.character(Team_Batting), 
+                               newBowlTeamId = as.character(Team_Bowling) ) %>%
+  filter (newBatTeamId < "99") %>% mutate (Team_Id = as.numeric(newBatTeamId)) %>%
+  left_join (teams, by = "Team_Id") %>% 
+  select (TeamNameBat = Team_Name, Team_Batting:Ball_id, cumOver:winner, 
+          Team_Bowling, newBowlTeamId) %>%
+  mutate ( Team_Id = as.numeric(newBowlTeamId)) %>% left_join (teams, by = "Team_Id") %>%
+  select(TeamNameBat:winner, TeamNameBowl = Team_Name, Team_Bowling)
+
+matDetChar <- matDet %>% mutate (newTeamId = as.character (Team_Batting)) %>% filter (newTeamId > "99")
+# IDs are already stored as names for Batting and Bowling teams for this set if rows in the original data file
+matDetChar <- matDetChar %>% mutate (TeamNameBat = Team_Batting, TeamNameBowl = Team_Bowling) %>%
+  select(TeamNameBat, Team_Batting:Ball_id, cumOver:winner, TeamNameBowl, Team_Bowling)
+# Now combine the 2 data.frames back into 1, and then save in each row if TeamNameBat won the match:
+matDet <- rbind(matDetNum, matDetChar) %>%
+  mutate (TeamBattingWon = as.character(TeamNameBat == winner)) %>%
+  arrange(TeamNameBat, Season, Match_id, Innings_No, Over_id, Ball_id)
+
+# This completes the ball by ball data file preparation. Let's write it out for data exploration 
+# writing to current working directory which is defined in the variable filesDir
+
+write.csv (matDet, "wrangled_ballByBallDataIPL.csv")
+
 # Now cull out only the rows pertaining to end of 6th (end of PowerPlay),10th, 15th and 20th overs to get the match situations (runs, wickets) at that stage
 # Since an innings may end before the scheduled 20 overs (all out before that or crossed opponenet score before that), determine when End of Innings happens
 # Also remember that not every over will end after 6 balls: extras can make an over go longer. Hence determine which was truly the last ball of an over
@@ -82,63 +116,53 @@ matDet17Over <- matDet %>% group_by (Team_Batting, Season, Match_id, Innings_No)
 # After creating these summaries at end of specific overs to be analyzed, keep only 1 row per match innings
 # So keep only Over = 1 and Ball = 1 for each match innings, and add those over stats as columns 
 
-matDet <- matDet %>% filter (Over_id == 1 & Ball_id == 1) %>%
+matSumm <- matDet %>% filter (Over_id == 1 & Ball_id == 1) %>%
   left_join(matDetEOI, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Innings_No, Team_Bowling,  
+  select (TeamNameBat:TeamBattingWon,  
           EOIOver = Over_id.y, EOIBall = Ball_id.y, EOIRuns = cumRuns.y, EOIWkts = cumWkts.y) %>% 
   ## if an innings lasts less than 12 overs, I dont want it to be included in the analysis: innings too short
   ## unless 9 or more wickets are lost in that innings (that is a full innings, just that hey could not bat even 12 overs)
   filter (EOIOver >= 12 )  %>%  ## | EOIWkts > 8
+  mutate (EOIRunRate = round(EOIRuns / EOIOver, 2)) %>%
   left_join(matDet4Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:EOIWkts, 
+  select (TeamNameBat:EOIRunRate, 
           Over4 = Over_id, Over4Ball = Ball_id, Over4Runs = cumRuns, Over4Wkts = cumWkts) %>%
+  mutate (Over4RunRate = round(Over4Runs / 4, 2)) %>%
   left_join(matDet6Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over4Wkts, 
+  select (TeamNameBat:Over4RunRate, 
           Over6 = Over_id, Over6Ball = Ball_id, Over6Runs = cumRuns, Over6Wkts = cumWkts) %>%
+  mutate (Over6RunRate = round(Over6Runs / 6, 2)) %>%
   left_join(matDet8Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over6Wkts, 
+  select (TeamNameBat:Over6RunRate, 
           Over8 = Over_id, Over8Ball = Ball_id, Over8Runs = cumRuns, Over8Wkts = cumWkts) %>%
+  mutate (Over8RunRate = round(Over8Runs / 8, 2)) %>%
   left_join(matDet10Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over8Wkts, 
+  select (TeamNameBat:Over8RunRate, 
           Over10 = Over_id, Over10Ball = Ball_id, Over10Runs = cumRuns, Over10Wkts = cumWkts) %>%
+  mutate (Over10RunRate = round(Over10Runs / 10, 2)) %>%
   left_join(matDet12Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over10Wkts, 
+  select (TeamNameBat:Over10RunRate, 
           Over12 = Over_id, Over12Ball = Ball_id, Over12Runs = cumRuns, Over12Wkts = cumWkts) %>%
+  mutate (Over12RunRate = round(Over12Runs / 12, 2)) %>%
   left_join(matDet15Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over12Wkts, 
+  select (TeamNameBat:Over12RunRate, 
           Over15 = Over_id, Over15Ball = Ball_id, Over15Runs = cumRuns, Over15Wkts = cumWkts) %>%
+  mutate (Over15RunRate = round(Over15Runs / 15, 2)) %>%
   left_join(matDet17Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over15Wkts, 
+  select (TeamNameBat:Over15RunRate, 
           Over17 = Over_id, Over17Ball = Ball_id, Over17Runs = cumRuns, Over17Wkts = cumWkts) %>%
+  mutate (Over17RunRate = round(Over17Runs / 17, 2)) %>%
   left_join(matDet20Over, by = c("Team_Batting", "Season", "Match_id", "Innings_No")) %>%
-  select (Team_Batting:Over17Wkts, 
+  select (TeamNameBat:Over17RunRate, 
           Over20 = Over_id, Over20Ball = Ball_id, Over20Runs = cumRuns, Over20Wkts = cumWkts) %>%
-  left_join(matSumm, by = c("Match_id" = "match_id")) %>%
-  select(Team_Batting:Over20Wkts, venueGround = Venue_Name, venueCity = City_Name, winner = match_winner)
+  mutate (Over20RunRate = round(Over20Runs / 20, 2)) %>%
+  select(TeamNameBat:Innings_No, venueGround:Over20RunRate) %>%
+  arrange(TeamNameBat, Season, Match_id, Innings_No)
 
-# Now add the batting and bowling team names with the lookup table team; Trouble with data "as is" is that 
-# seasons 2008-2016 has numbers in the batting and bolwing team ids, but for season 2017, the actual team names are stored
-# So do the lookup in 2 steps: first for the nueric ids (they actually are factor data types)
 
-matDetNum <-matDet %>% mutate (newBatTeamId = as.character(Team_Batting), newBowlTeamId = as.character(Team_Bowling) ) %>%
-  filter (newBatTeamId < "99") %>% mutate (Team_Id = as.numeric(newBatTeamId)) %>%
-  left_join (teams, by = "Team_Id") %>% 
-  select (TeamName_Bat = Team_Name, Team_Batting:Innings_No, EOIOver:winner, 
-           Team_Bowling, newBowlTeamId) %>%
-  mutate ( Team_Id = as.numeric(newBowlTeamId)) %>% left_join (teams, by = "Team_Id") %>%
-  select(TeamName_Bat:winner, TeamName_Bowl = Team_Name, Team_Bowling)
-
-matDetChar <- matDet %>% mutate (newTeamId = as.character (Team_Batting)) %>% filter (newTeamId > "99")
-# IDs are already stored as names for Batting and Bowling teams for this set if rows in the original data file
-matDetChar <- matDetChar %>% mutate (TeamName_Bat = Team_Batting, TeamName_Bowl = Team_Bowling) %>%
-  select(TeamName_Bat, Team_Batting:Innings_No, EOIOver:winner, TeamName_Bowl, Team_Bowling)
-# Now combine the 2 data.frames back into 1:
-matDet <- rbind(matDetNum, matDetChar) %>%
-  arrange(TeamName_Bat, Season, Match_id, Innings_No)
-
-#Finally write the new data frame to a csv file so that we can do exploration and then create predictive model
-#writing to current working directory which is defined in teh variable filesDir
-write.csv (matDet, "wrangled_ballByBallData.csv")
+#Finally write the new summary data frame to a csv file so that we can do exploration and then create predictive model
+#writing to current working directory which is defined in the variable filesDir
+write.csv (matSumm, "wrangled_matchSummaryDataIPL.csv")
 
 
 # build new data frame for analysis from sorted data files for 2008
